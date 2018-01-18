5cca04f4a039d4b6b105f5939956cf4fbfb30de0
marcusw <marcusw@pageuppeople.com>
2018-01-19 10:45:36 +1100

    Add Google Analytics

--- includes/Header.php
+++ includes/Header.php
@@ -1,4 +1,13 @@
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112755463-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-112755463-1');
+	</script>
 	<title><?php echo $pageTitle ?></title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
