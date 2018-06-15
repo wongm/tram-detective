d045a2043cfff9f9803ae7a25ce952b4f7a60724
Marcus Wong <marcusthomaswong@gmail.com>
2018-06-15 20:58:20 +1000

    Fix 'find a tram' link

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ $pageTitle = "Gunzel Operations Center";
 $pageDescription = "Tracking the trams of Melbourne";
 require_once('includes/Header.php');
 ?>
-<form action="tram/" method="get">
+<h2>Find a tram</h2>
+<form action="tram.php" method="get">
 <label for="id">Tram number</label>
 <input type="number" id="id" name="id" maxlength="4" size="4" />
 <input type="submit" value="Find it!" /></br>
